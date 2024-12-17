8340def91b3df5a718b7a03ee0bde6c33b2f81de
JulianeMaran32 <julianemaran@gmail.com>
2024-12-16 21:53:48 -0300

    projeto sintaxe basica com javadoc

---
+++ CoffeeCode/sintaxe/HelloWorld.java
@@ -0,0 +1,28 @@
+package sintaxe;
+
+/**
+ * Classe que representa um programa simples em Java que imprime a mensagem "Hello World!" no console.
+ * <p>
+ * Esta classe contém apenas o metodo principal {@code main},
+ * que é responsável por executar o programa e imprimir a mensagem.
+ *
+ * @author Juliane Maran
+ * @version 1.0
+ * @since 2024-12-16
+ */
+public class HelloWorld {
+    /**
+     * Metodo principal que inicia a execução do programa.
+     * <p>
+     * O metodo {@code main} imprime no console a mensagem "Hello World!".
+     * É o ponto de entrada padrão para execução de programas em Java.
+     *
+     * @param args Um array de strings que permite a passagem de argumentos de linha de comando
+     *             (não utilizado neste exemplo).
+     */
+    public static void main(String[] args) {
+        // Imprime a mensagem "Hello World!" no console
+        System.out.println("Hello World!");
+    }
+
+}
