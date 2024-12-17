770fe6146b10c498ac5b8c871ff87a45acc0f93c
JulianeMaran32 <julianemaran@gmail.com>
2024-12-17 08:44:37 -0300

    demonstração de escopo

---
+++ codecoffe/sintaxes/Escopo.java
@@ -0,0 +1,21 @@
+package sintaxes;
+
+public class Escopo {
+
+    public static void main(String[] args) {
+        int i = 5;
+        {
+            int j = 0;
+            System.out.println(i);
+            j = 5 * i;
+            System.out.println(j);
+            {
+                System.out.println(i);
+                System.out.println(j);
+            }
+        }
+        System.out.println(i);
+        // System.out.println(j);
+    }
+
+}
