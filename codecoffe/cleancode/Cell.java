0ae657001116457fd423d98e2049fa9772587676
JulianeMaran32 <julianemaran@gmail.com>
2024-12-17 12:31:54 -0300

    refatorando codigo com nomes que revelem seu proposito

---
+++ codecoffe/cleancode/Cell.java
@@ -0,0 +1,23 @@
+package cleancode;
+
+public class Cell {
+
+    private int status;
+
+    public Cell(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public boolean isFlagged() {
+        return this.status == CellStatus.FLAGGED;
+    }
+
+}
