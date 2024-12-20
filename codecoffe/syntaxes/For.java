7efd57531287e7fd1a201399a9a49ffe2cf4a5c8
JulianeMaran32 <julianemaran@gmail.com>
2024-12-20 15:22:40 -0300

    Uso da diretiva for

---
+++ codecoffe/syntaxes/For.java
@@ -0,0 +1,14 @@
+package syntaxes;
+
+/**
+ * Uso da diretiva `for` que realiza a contagem de 0 até 9, ou seja, 10 iterações
+ */
+public class For {
+
+    public static void main(String[] args) {
+        int j;
+        for (j = 0; j < 10; j++)
+            System.out.println(j);
+    }
+
+}
