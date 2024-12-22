ed071d4262453db6bb69fa994945a4e0dbb5b5d0
JulianeMaran32 <julianemaran@gmail.com>
2024-12-21 21:40:27 -0300

    Report Config coretirado os comentários e incluido espaçamentos

--- cleancode/src/main/java/br/com/juhmaran/cleancode/formatting/ReporterConfig.java
+++ cleancode/src/main/java/br/com/juhmaran/cleancode/formatting/ReporterConfig.java
@@ -6,13 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReporterConfig {
-    /**
-     * The class name of the reporter listener
-     */
+
     private String m_className;
-    /**
-     * The properties of the reporter listener
-     */
+
     private List<Property> m_properties = new ArrayList<Property>();
 
     public void addProperty(Property property) {
